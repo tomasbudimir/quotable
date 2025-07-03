0dfac497fe5f30a3e01c5bcc08a6731125647597
Tomas Budimir <tomas.budimir@gmail.com>
2025-07-03 20:03:18 +0200

    Some performance improvements

--- src/app/tabs/one-quote/one-quote.page.ts
+++ src/app/tabs/one-quote/one-quote.page.ts
@@ -3,11 +3,7 @@ import { Router } from '@angular/router';
 import { DataService } from '../../services/data.service';
 import { QuoteRecord } from '../../models/quote-record';
 import { AuthService } from '../../services/auth.service';
-import { AlertService } from '../../services/alert.service';
 import { FontSizeService } from '../../services/font-size.service';
-import { ModalController } from '@ionic/angular';
-import { ModalLoginPage } from '../../auth/modal-login/modal-login.page';
-import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-one-quote',
@@ -17,67 +13,38 @@ import html2canvas from 'html2canvas';
 })
 export class OneQuotePage {
   @ViewChild('myCanvas', { static: false }) myCanvas!: ElementRef<HTMLCanvasElement>;
+  quotes: QuoteRecord[] = [];
   quote: QuoteRecord = null;
 
   constructor(private router: Router,
     private authService: AuthService,
     private dataService: DataService,
-    private alertService: AlertService,
-    private fontSizeService: FontSizeService,
-    private modalController: ModalController
+    private fontSizeService: FontSizeService
   ) { }
 
   ionViewDidEnter() {
     this.dataService.getQuotes().subscribe(res => {
       if (res) {
-        const index = Math.floor(Math.random() * res.length);
+        this.quotes = res;
 
-        this.quote = res[index];
+        const index = this.getIndex(res.length);
+        this.quote = this.quotes[index];
       } else {
         this.router.navigate(['/tabs']);
       }
     });
   }
 
-  get isLoggedIn(): boolean {
-    return this.authService.user != null;
-  }
-
   getFontSize(quoteText: string): number {
     return this.fontSizeService.getBiggerFontSize(quoteText);
   }
 
-  getLikeIconName(quote: QuoteRecord): string {
-    if (this.isQuoteLikedByCurrentUser(quote)) {
-      return 'heart';
-    }
-
-    return 'heart-outline';
+  getIndex(size: number): number {
+    return Math.floor(Math.random() * size);
   }
 
-  isMyPost(quote: QuoteRecord): boolean {
-    return this.isLoggedIn 
-      && this.authService.user?.uid == quote.uid;
-  }
-
-  isQuoteLikedByCurrentUser(quote: QuoteRecord): boolean {
-    if (quote?.likes?.includes(this.authService?.user?.uid)) {
-      return true;
-    }
-
-    return false;
-  }
-
-  async downloadImage(quote: QuoteRecord) {
-    const element = document.getElementById(quote.id);
-    if (!element) return;
-
-    await html2canvas(element).then((canvas) => {
-      const image = canvas.toDataURL('image/png');
-      const link = document.createElement('a');
-      link.href = image;
-      link.download = quote.quoteText.substring(0, 20) + '.png';
-      link.click();
-    });
+  showAnother() {
+      const index = this.getIndex(this.quotes.length);
+      this.quote = this.quotes[index];
   }
 }
