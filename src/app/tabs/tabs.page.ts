9a16d165fab7c9da6f54bab86e6aa22c3474c3cd
Tomas Budimir <tomas.budimir@gmail.com>
2025-07-03 20:12:24 +0200

    few more improvements

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,5 +1,6 @@
 import { AuthService } from './../services/auth.service';
 import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { IonTabs } from '@ionic/angular';
 
 @Component({
@@ -11,7 +12,14 @@ import { IonTabs } from '@ionic/angular';
 export class TabsPage {
   @ViewChild('tabs') tabs: IonTabs;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, 
+    private router: Router) { }
+
+  ionViewWillEnter() {
+    if (this.router.url.toLowerCase().endsWith('tabs')) {
+      this.tabs.select('home');
+    }
+  }
 
   get isLoggedIn(): boolean {
     return this.authService.user != null;
