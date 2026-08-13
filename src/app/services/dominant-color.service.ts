c2039c8f18c338113a1f1c652e81fcc0bc74528f
Tomas Budimir <tomasbudimir@gmail.com>
2026-08-13 12:41:48 +0200

    Minor changes

--- src/app/services/dominant-color.service.ts
+++ src/app/services/dominant-color.service.ts
@@ -54,11 +54,11 @@ export class DominantColorService {
   getContrastingColor({ r, g, b }: RGB): string {
     const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
 
-    if (luminance < 0.6) {
+    if (luminance < 0.5) {
       return '#FFFFFF';
     }
 
-    if (luminance < 0.8) {
+    if (luminance < 0.6) {
       return '#FFDD00';
     }
 
